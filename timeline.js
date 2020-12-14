392cafbd790aab8da17cc069a07ef108815f5074
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2020-12-14 11:11:00 -0300

    timeline funcionando

--- timeline.js
+++ timeline.js
@@ -1,1 +1,23 @@
-var timelineSteps = document.querySelectorAll (".timeline-steps");
+var stepsList = document.querySelector(".steps-list");
+
+var progressBarSteps = document.querySelectorAll (".progress-bar-step");
+
+var progressBarStepsArr = Array.from(progressBarSteps);
+
+progressBarStepsArr[0].addEventListener("click", function(){
+	stepsList.style.marginLeft = 0;
+});
+
+progressBarStepsArr[1].addEventListener("click", function(){
+	stepsList.style.marginLeft = "-100%";
+});
+
+progressBarStepsArr[2].addEventListener("click", function(){
+	stepsList.style.marginLeft = "-200%";
+});
+
+progressBarStepsArr[3].addEventListener("click", function(){
+	stepsList.style.marginLeft = "-300%";
+});
+
+
