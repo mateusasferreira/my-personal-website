05666ef556d6886a610e0693a460735d33d0b663
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2020-12-14 16:36:41 -0300

    progress bar funcionando

--- timeline.js
+++ timeline.js
@@ -5,19 +5,32 @@ var progressBarSteps = document.querySelectorAll (".progress-bar-step");
 var progressBarStepsArr = Array.from(progressBarSteps);
 
 progressBarStepsArr[0].addEventListener("click", function(){
-	stepsList.style.marginLeft = 0;
+	stepsList.style.marginLeft = 0;	
+	progressBarStepsArr[1].classList.remove("fulfill");
+	progressBarStepsArr[2].classList.remove("fulfill");
+	progressBarStepsArr[3].classList.remove("fulfill");
 });
 
 progressBarStepsArr[1].addEventListener("click", function(){
 	stepsList.style.marginLeft = "-100%";
+	progressBarStepsArr[1].classList.add("fulfill");
+	progressBarStepsArr[2].classList.remove("fulfill");
+	progressBarStepsArr[3].classList.remove("fulfill");
 });
 
 progressBarStepsArr[2].addEventListener("click", function(){
 	stepsList.style.marginLeft = "-200%";
+	progressBarStepsArr[1].classList.add("fulfill");
+	progressBarStepsArr[2].classList.add("fulfill");
+	progressBarStepsArr[3].classList.remove("fulfill");
+
 });
 
 progressBarStepsArr[3].addEventListener("click", function(){
 	stepsList.style.marginLeft = "-300%";
+	progressBarStepsArr[1].classList.add("fulfill");
+	progressBarStepsArr[2].classList.add("fulfill");
+	progressBarStepsArr[3].classList.add("fulfill");
 });
 
 
