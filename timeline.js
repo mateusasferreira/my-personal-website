9ca25f605c777d0676a00d58951df547de2fb3b1
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2020-12-15 10:32:09 -0300

    timeline melhorada, looping implementado

--- timeline.js
+++ timeline.js
@@ -4,6 +4,23 @@ var progressBarSteps = document.querySelectorAll (".progress-bar-step");
 
 var progressBarStepsArr = Array.from(progressBarSteps);
 
+progressBarStepsArr.forEach(function(step){
+	step.addEventListener("click", function(){
+		let index = progressBarStepsArr.indexOf(step);
+		changeStep(index);})
+
+})
+
+function changeStep (i) {
+	let operator = i * -100;
+	stepsList.style.marginLeft = operator + "%";
+}
+
+
+
+
+
+/*
 progressBarStepsArr[0].addEventListener("click", function(){
 	stepsList.style.marginLeft = 0;	
 	progressBarStepsArr[1].classList.remove("fulfill");
@@ -32,5 +49,5 @@ progressBarStepsArr[3].addEventListener("click", function(){
 	progressBarStepsArr[2].classList.add("fulfill");
 	progressBarStepsArr[3].classList.add("fulfill");
 });
-
+*/
 
