fa254a06afffb274a99b463e5fa2b970488a4ece
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2021-05-31 15:35:50 -0300

    corrected variable type

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -6,13 +6,13 @@ const progressBarStepsArr = Array.from(progressBarSteps);
 
 progressBarStepsArr.forEach(function(step){
 	step.addEventListener("click", function(){
-		let index = progressBarStepsArr.indexOf(step);
+		const index = progressBarStepsArr.indexOf(step);
 		changeStep(index);
-		let checkedSteps = progressBarStepsArr.slice(0, index + 1);
+		const checkedSteps = progressBarStepsArr.slice(0, index + 1);
 		checkedSteps.forEach(function(checkedStep){
 			checkedStep.classList.add("active");	
 		})
-		let uncheckedSteps = progressBarStepsArr.slice(index + 1, progressBarStepsArr.length + 1);
+		const uncheckedSteps = progressBarStepsArr.slice(index + 1, progressBarStepsArr.length + 1);
 		uncheckedSteps.forEach(function(uncheckedStep){
 			uncheckedStep.classList.remove("active")
 		})
@@ -22,43 +22,8 @@ progressBarStepsArr.forEach(function(step){
 })
 
 function changeStep (i) {
-	let operator = i * -100;
+	const operator = i * -100;
 	stepsList.style.marginLeft = operator + "%";
 }
 
-
-//This below is how I originally wrote the code above, just before I managed to implement a looping;
-// Abaixo é como eu escrevi o código acima originalmente, antes de implementar o looping;
-
-
-/*
-progressBarStepsArr[0].addEventListener("click", function(){
-	stepsList.style.marginLeft = 0;	
-	progressBarStepsArr[1].classList.remove("fulfill");
-	progressBarStepsArr[2].classList.remove("fulfill");
-	progressBarStepsArr[3].classList.remove("fulfill");
-});
-
-progressBarStepsArr[1].addEventListener("click", function(){
-	stepsList.style.marginLeft = "-100%";
-	progressBarStepsArr[1].classList.add("fulfill");
-	progressBarStepsArr[2].classList.remove("fulfill");
-	progressBarStepsArr[3].classList.remove("fulfill");
-});
-
-progressBarStepsArr[2].addEventListener("click", function(){
-	stepsList.style.marginLeft = "-200%";
-	progressBarStepsArr[1].classList.add("fulfill");
-	progressBarStepsArr[2].classList.add("fulfill");
-	progressBarStepsArr[3].classList.remove("fulfill");
-
-});
-
-progressBarStepsArr[3].addEventListener("click", function(){
-	stepsList.style.marginLeft = "-300%";
-	progressBarStepsArr[1].classList.add("fulfill");
-	progressBarStepsArr[2].classList.add("fulfill");
-	progressBarStepsArr[3].classList.add("fulfill");
-});
-*/
 
